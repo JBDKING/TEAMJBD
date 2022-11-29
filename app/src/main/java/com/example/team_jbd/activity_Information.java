26d4d98466b4d4ae0acba56a4bf0c091825193c4
JBDKING <w9758@naver.com>
2022-11-29 15:11:00 +0900

    Update

--- app/src/main/java/com/example/team_jbd/activity_Information.java
+++ app/src/main/java/com/example/team_jbd/activity_Information.java
@@ -24,14 +24,16 @@ import com.google.firebase.database.FirebaseDatabase;
 public class activity_Information extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
     Button button;
-    Spinner spinner;
+    Spinner spinner1, spinner2;
+    TextView textView1, textView2;
     DatabaseReference databaseReference;
-    String item;
+    String item1, item2;
     activity_Member member;
 
     Context mContext;
 
     String[] pet = {"선택","웰시코기","시바견","말티즈","푸들","포메라니안","비숑"};
+    String[] wei = {"선택","1","2","3","4","5","6"};
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -40,24 +42,38 @@ public class activity_Information extends AppCompatActivity implements AdapterVi
         setContentView(R.layout.activity_information);
         mContext = this;
 
+        textView1 = findViewById(R.id.dog);
+        textView2 = findViewById(R.id.wei);
+
         button = findViewById(R.id.button_save);
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference("Value");
-        spinner = findViewById(R.id.spinner);
-        spinner.setOnItemSelectedListener(this);
+
+        spinner1 = findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(this);
+
+        spinner2 = findViewById(R.id.spinner2);
+        spinner2.setOnItemSelectedListener(this);
 
         member = new activity_Member();
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,pet);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter arrayAdapter1 = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,pet);
+        arrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spinner.setAdapter(arrayAdapter);
+        ArrayAdapter arrayAdapter2 = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,wei);
+        arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner1.setAdapter(arrayAdapter1);
+        spinner2.setAdapter(arrayAdapter2);
 
         button.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View view) {
-                saveValue(item);
+            public void onClick(View view)
+            {
+                saveValue(item1);
+                saveValue(item2);
             }
         });
     }
@@ -65,7 +81,11 @@ public class activity_Information extends AppCompatActivity implements AdapterVi
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
     {
-        item = spinner.getSelectedItem().toString();
+        item1 = spinner1.getSelectedItem().toString();
+        textView1.setText(item1);
+
+        item2 = spinner2.getSelectedItem().toString();
+        textView2.setText(item2);
     }
 
     @Override
--- app/src/main/java/com/example/team_jbd/activity_Member.java
+++ app/src/main/java/com/example/team_jbd/activity_Member.java
@@ -3,19 +3,25 @@ package com.example.team_jbd;
 public class activity_Member
 {
     private String pet;
+    private String wei;
 
     public activity_Member()
     {
 
     }
-
-    public String getPet()
-    {
+    public String getPet() {
         return pet;
     }
 
-    public void setPet(String pet)
-    {
+    public void setPet(String pet) {
         this.pet = pet;
+    }
+
+    public String getWei() {
+        return wei;
+    }
+
+    public void setWei(String wei) {
+        this.wei = wei;
     }
 }
